61991c318b5debed44bb52ad494367389606b288
Jonathan Abi <jonathanabi81@gmail.com>
2023-12-20 19:07:14 +0700

    email validation

--- handlers/registerHandler.js
+++ handlers/registerHandler.js
@@ -15,12 +15,19 @@ async function registerHandler(req, res) {
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    const [results] = await pool.query('INSERT INTO users (email, name, password) VALUES (?, ?, ?)', [email, name, hashedPassword]);
-    res.status(201).json({ status: 201, message: 'User registered successfully' });
+    const [existingUser] = await pool.query('SELECT * FROM users WHERE email = ?', [email]);
+  
+    if (existingUser.length > 0) {
+      res.status(409).json({ status: 409, message: 'Email already exists' });
+    } else {
+      const [results] = await pool.query('INSERT INTO users (email, name, password) VALUES (?, ?, ?)', [email, name, hashedPassword]);
+      res.status(201).json({ status: 201, message: 'User registered successfully' });
+    }
   } catch (error) {
     console.error('Error registering user:', error);
     res.status(500).json({ status: 500, message: 'Internal Server Error' });
   }
+  
 }
 
 module.exports = registerHandler;
